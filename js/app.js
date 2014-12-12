1ffa98d90b90f445d736b3415a527a82a927e8c5
smshin7 <scottshin1@gmail.com>
2014-12-12 11:41:49 -0800

    pushing all files to github

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// var boxOne = document.getElementById("box1");
+// var boxTwo = document.getElementById("box2");
+// var boxThree = document.getElementById("box3");
+// var boxFour = document.getElementById("box4");
+// var boxFive = document.getElementById("box5");
+// var boxSix = document.getElementById("box6");
+// var boxSeven = document.getElementById("box7");
+// var boxEight = document.getElementById("box8");
+// var boxNine = document.getElementById("box9");
+
+// this functions will run when button is clicked
+function flipCoin() {
+
+var coinValue;
+var	coinMath = Math.floor();
+	if (coinMath >= 0.50)
+		//set coinValue to heads
+		coinValue = "Heads";
+		return coinValue;
+	} else {
+		//set coinValue to tails
+		coinValue = "Tails";
+}
+

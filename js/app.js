bee448a3b90f75437560d21b17d26cf4974db6bf
smshin7 <scottshin1@gmail.com>
2014-12-12 17:57:40 -0800

    redesigned gameboard and added scoreboard

--- js/app.js
+++ js/app.js
@@ -1,36 +1,18 @@
-// var boxOne = document.getElementById("box1");
-// var boxTwo = document.getElementById("box2");
-// var boxThree = document.getElementById("box3");
-// var boxFour = document.getElementById("box4");
-// var boxFive = document.getElementById("box5");
-// var boxSix = document.getElementById("box6");
-// var boxSeven = document.getElementById("box7");
-// var boxEight = document.getElementById("box8");
-// var boxNine = document.getElementById("box9");
 
-// this function will run when button is clicked
-function flipCoin() {
-	var	coinMath = Math.random();
-	if (coinMath > 0.50) {
-		//set coinValue to heads
-		document.getElementById("coin").innerHTML = "HEADS";
-		return "heads";
+// // this function will run when button is clicked
+// function flipCoin() {
+// 	var	coinMath = Math.random();
+// 	if (coinMath > 0.50) {
+// 		//set coinValue to heads
+// 		document.getElementById("coin").innerHTML = "HEADS";
+// 		return "heads";
 		
-	} else {
-		//set coinValue to tails
-		document.getElementById("coin").innerHTML = "TAILS";
-		return "tails";
-	}
-};
-
-
-// this function will push value into array X or O
-function selectSquare () {
-
-
-
-
-}
+// 	} else {
+// 		//set coinValue to tails
+// 		document.getElementById("coin").innerHTML = "TAILS";
+// 		return "tails";
+// 	}
+// };
 
 
 // ANGULAR APP
@@ -40,6 +22,47 @@ var tttApp = angular.module('tttApp',[]);
 tttApp.controller('TTTController' ['$scope', function($scope) {
 
 
+//this is the empty arrays for player 1
+
+
+//this is the empty arrays for player 2
+
+// this function will push property true into arraw
+function selectSquareOne () {
+	$scope.playerOne.push();
+	}
+
+function selectSquareTwo () {
+	$scope.playerOne.push();
+}
+
+function selectSquareThree () {
+	$scope.playerOne.push();
+}
+
+function selectSquareFour () {
+	$scope.playerOne.push();
+}
+
+function selectSquareFive () {
+	$scope.playerOne.push();
+}
+
+function selectSquareSix () {
+	$scope.playerOne.push();
+}
+
+function selectSquareSeven () {
+	$scope.playerOne.push();
+}
+
+function selectSquareEight () {
+	$scope.playerOne.push();
+}
+
+function selectSquareNine () {
+	$scope.playerOne.push();
+}
 
 
 

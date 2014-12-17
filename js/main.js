df32f5c0b941aad8872a61f41f05f880a3b92547
smshin7 <scottshin1@gmail.com>
2014-12-16 19:16:25 -0800

    added switch players on click

--- js/main.js
+++ js/main.js
@@ -20,31 +20,45 @@ var gameApp = angular.module('gameApp',[]);
 // functions for the controller go in here
 gameApp.controller('GameController', ['$scope', function($scope) {
 
+	$scope.currentPlayer = 'x';
+	$scope.playerOne = 'x';
+	$scope.playerTwo = 'o';
+	$scope.catsGame = false;
 
+
+// this is the grid to set up the gameboard
 	$scope.grid = [
 									[{pick: ''}, {pick: ''}, {pick: ''}],
 									[{pick: ''}, {pick: ''}, {pick: ''}],
 									[{pick: ''}, {pick: ''}, {pick: ''}]
 								];
 
+// ng-click selects the square
 	$scope.selectSquare = function() {
-		
+		console.log($scope.currentPlayer);
+		console.log($scope.grid);
 
-	}							
+		if ($scope.currentPlayer == 'x') {
+			this.cell.pick = $scope.playerOne;
+			$scope.currentPlayer = $scope.playerTwo;
+		} else {
+			this.cell.pick = $scope.playerTwo;
+			$scope.currentPlayer = $scope.playerOne ;
+			};
+		return $scope.currentPlayer;
+	};				
+
+// this function determines whos the currentplayer
+ // $scope.currentPlayer = function() {
+ // 	if(playerOne )
+
+ // };
 
 
+// this function declares winner
+	// $scope.evaluateWin = function(){
+	// 	if (catsGame == false && grid [][] )
 
-	// $scope.selectCell = function(index) {
-	// 	if ((cells = '') && (playerOne = true)) {
-	// 		$scope.index = 'x';
-	// 		} else {
-	// 			$scope.index = 'o'
-	// 	}
-
-	// 		console.log(index);
 	// };
-
-
-
 	
 	}]);
